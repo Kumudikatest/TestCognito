4966fabe3692537a6a5e1f100e0c96a42f290192
Kumudikatest <37790188+Kumudikatest@users.noreply.github.com>
2018-05-10 14:54:58 +0530

    TestCognito

--- TestCognito/lambda.js
+++ TestCognito/lambda.js
@@ -8,7 +8,7 @@ const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 exports.handler = function (event, context, callback) {
 	cognito_idp.listUsers({
 		UserPoolId: process.env.UserPoolId_cognitoKTestCognito,
-		Filter: 'email="kumudika@adroitlogic.com"'
+		Filter: 'email="kumudika+@adroitlogic.com"'
 	}, function (error, data) {
 		if (error) {
 			// implement error handling logic here
